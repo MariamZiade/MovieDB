a92cc62ee4b687b960cf474e43c885464b59af14
MariamZiade <mariam.ziade@gmail.com>
2021-04-06 01:22:07 +0300

    Step 3 Completed

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
+
+
 const http = require('http')
+const express = require('express')
 const port = 3000
+const app = express()
+const today = new Date();
+const time = today.getHours() + ":" + today.getSeconds();
 
 const server = http.createServer(function(req, res) {
-res.write('ok')
+res.write('mariam')
 res.end()
 })
+app.get('/', (req, res) => res.send('ok')) 
+app.get('/test', (req, res) => res.send({status:200, message:"ok"}))
+app.get('/time', (req, res) => res.send({status:200, message: time}))
+
 
 server.listen(port, function(error) {
 if (error) {
